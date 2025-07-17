bbeb36799cea2eb5e5693da86b294f43d9a8153c
Finn1226 <yifeili1226@gmail.com>
2025-07-17 16:17:30 +0800

    created home page, quotes page, and the _layout for these 2 pages

---
+++ app/quotes.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function QuotesPage() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Quotes Pages</Text>
+    </View>
+  );
+}
